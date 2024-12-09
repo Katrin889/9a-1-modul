928b204bd98805b78071efba2d4d4b59a9d34ab3
Katrin889 <kakamitreva@gmail.com>
2024-12-09 13:39:58 +0200

    Add files via upload

---
+++ Hokin/Klukarka/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Klukarka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете 1 буква от име ");
+            var a = Console.ReadLine();
+            switch (a)
+            {
+                case "Р": Console.WriteLine("Вечно блее в час"); break;
+                case "Х": Console.WriteLine("Клюкарка"); break;
+                case "М":
+                case "С":
+                case "Н":
+                case "К":
+                case "Ю":
+                case "х": Console.WriteLine("Само те слушат"); break;
+                default: Console.WriteLine("Няма други"); break;
+            }
+        }
+    }
+}
---
+++ Hokin/Месец/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Месец
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи мессец ");
+            var a = Console.ReadLine();
+            var b = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case "2":
+                case "1":
+                case "12": Console.WriteLine("zima"); break;
+               // case "3":
+               //     if (b < 29) { Console.WriteLine("Високосна година"); }
+                case "4":
+                case "5": Console.WriteLine("Пролет"); break;
+                case "6":
+                case "7":
+                case "8": Console.WriteLine("Лято"); break;
+                case "9":
+                case "10":
+                case "11": Console.WriteLine("Есен"); break;
+                default: Console.WriteLine("Ne e mesec"); break;
+            }
+        }
+    }
+}
